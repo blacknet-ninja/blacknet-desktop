cc6b292d6ad772b46bef0c2d89ad14734023198b
blacknet1fcemzzxna <blacknet1fcemzzxna@gmail.com>
2019-08-16 18:34:32 +0800

    update

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-version=`cat version`
+version='0.2.3'
 
 dmgsha256=`shasum -a 256 dist/Blacknet-${version}.dmg`
 winsha256=`shasum -a 256 dist/Blacknet\ Setup\ ${version}.exe`
@@ -9,5 +9,5 @@ winsha256=${winsha256:1:64}
 echo $dmgsha256
 echo $winsha256
 
-curl --request POST --header "Private-Token: ${gitlab_private_token}" --form "file=@dist/Blacknet-${version}.dmg" "https://gitlab.COM/api/v4/projects/blacknet-ninja%2Fblacknet-desktop/uploads"
-curl --request POST --header "Private-Token: ${gitlab_private_token}" --form "file=@dist/Blacknet Setup ${version}.exe" "https://gitlab.COM/api/v4/projects/blacknet-ninja%2Fblacknet-desktop/uploads"
+curl --request POST --header "Private-Token: ${gitlab_private_token}" --form "file=@dist/Blacknet-${version}.dmg" "https://gitlab.COM/api/v4/projects/blacknet1fcemzzxna%2Fblacknet-desktop/uploads"
+curl --request POST --header "Private-Token: ${gitlab_private_token}" --form "file=@dist/Blacknet Setup ${version}.exe" "https://gitlab.COM/api/v4/projects/blacknet1fcemzzxna%2Fblacknet-desktop/uploads"
